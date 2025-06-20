6521431d9b494c4dc000de6ed93795456fba0e6b
unknown <nduflot@dawan.fr>
2025-06-20 14:57:21 +0200

    révisions js pt 1: generateur de personnage

---
+++ js/creaPerso.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function(){
+    /*
+    Pour pouvoir interagir avec les éléments HTML de la page, il faut attendre l'événement DOMContentLoaded du navigateur (window).
+    Comme cela, JS connaît tout l'arbre DOM de la page et on sera capable de surveiller des événement de la page (document)
+    */
+
+    
+});
